4ba96995ca9075ad3cb42505c717d13a82863ca9
Stephan Lukasczyk <stephan@lukasczyk.name>
2024-01-06 19:25:06 +0100

    Fix faulty language selection for mail

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -73,7 +73,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "mail" },
   callback = function()
     vim.opt_local.spell = true
-    vim.opt_local.spelllang = "de_de|en_gb"
+    vim.opt_local.spelllang = "de_de"
     vim.opt_local.textwidth = 72
   end,
 })
